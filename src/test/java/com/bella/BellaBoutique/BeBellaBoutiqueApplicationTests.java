6cb16d2713d8b961b65ee537f26710a7d2a5fc5a
Bojoura <bo.efendi@gmail.com>
2024-11-14 16:10:28 +0100

    database test

--- src/test/java/com/bella/BellaBoutique/BeBellaBoutiqueApplicationTests.java
+++ src/test/java/com/bella/BellaBoutique/BeBellaBoutiqueApplicationTests.java
@@ -1,1 +1,9 @@
+package com.bella.BellaBoutique;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBellaBoutiqueApplicationTests {
+    void contextLoads() {
+    }
+}
