a42179250dfe3bcde27b852ad658a88bff599c84
Bojoura <bo.efendi@gmail.com>
2025-01-09 14:53:03 +0100

    Minor change, remove a nonfunctional line

--- src/main/java/com.bella.BellaBoutique/exception/RecordNotFoundException.java
+++ src/main/java/com.bella.BellaBoutique/exception/RecordNotFoundException.java
@@ -2,10 +2,6 @@ package com.bella.BellaBoutique.exception;
 
 public class RecordNotFoundException extends RuntimeException {
 
-    public RecordNotFoundException() {
-        super();
-    }
-
     public RecordNotFoundException(String message) {
         super(message);
     }
