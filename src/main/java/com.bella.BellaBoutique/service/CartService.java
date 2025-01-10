6f77fad81b32fbc4844830d614d1e3b9e2345573
Bojoura <bo.efendi@gmail.com>
2025-01-10 20:18:52 +0100

    removed

--- src/main/java/com.bella.BellaBoutique/service/CartService.java
+++ src/main/java/com.bella.BellaBoutique/service/CartService.java
@@ -1,6 +1,6 @@
 //package com.bella.BellaBoutique.service;
+//import com.bella.BellaBoutique.model.cart.CartItem;
 //import com.bella.BellaBoutique.model.products.Product;
-//import com.bella.BellaBoutique.model.shopCart.CartItem;
 //import com.bella.BellaBoutique.model.shopCart.ShopCart;
 //import com.bella.BellaBoutique.repository.CartRepository;
 //import com.bella.BellaBoutique.repository.ProductRepository;
@@ -14,19 +14,19 @@
 //    private final CartRepository cartRepository;
 //    private final ProductRepository productRepository;
 //
-//    public void addProductToCart(Long productId, int quantity) {
-//        Product product = productRepository.findById(productId)
-//                .orElseThrow(() -> new RuntimeException("Product niet gevonden"));
-//
-//        ShopCart.Cart cart = cartRepository.findByUserId(1L);  // Voorbeeld user ID
-//        CartItem cartItem = cart.getItems().stream()
-//                .filter(item -> item.getProduct().getId().equals(productId))
-//                .findFirst()
-//                .orElse(new CartItem(product, 0));
-//
-//        cartItem.setQuantity(cartItem.getQuantity() + quantity);
-//        cart.getItems().add(cartItem);
-//        cartRepository.save(cart);
-//    }
+////    public void addProductToCart(Long productId, int quantity) {
+////        Product product = productRepository.findById(productId)
+////                .orElseThrow(() -> new RuntimeException("Product niet gevonden"));
+////
+////        ShopCart.Cart cart = cartRepository.findByUserId(1L);  // Voorbeeld user ID
+////        CartItem cartItem = cart.getItems().stream()
+////                .filter(item -> item.getProduct().getId().equals(productId))
+////                .findFirst()
+////                .orElse(new CartItem(product, 0));
+////
+////        cartItem.setQuantity(cartItem.getQuantity() + quantity);
+////        cart.getItems().add(cartItem);
+////        cartRepository.save(cart);
+////    }
 //}
 //
